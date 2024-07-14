75a163e3e599801e65d54b7065e89c90596b6f52
BigDarkness3232 <fre.cardenas@duocuc.cl>
2024-07-14 18:49:39 -0400

    Update ferramasFA.sql

--- ferramasFA.sql
+++ ferramasFA.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `ferramas`
 --
-
+CREATE DATABASE IF NOT EXISTS `Ferramas` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+USE `Ferramas`;
 -- --------------------------------------------------------
 
 --
